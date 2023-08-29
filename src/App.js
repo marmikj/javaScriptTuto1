8ca896982f0cb006612f6d23e518c0392e669806
marmikj <140173006+marmikj@users.noreply.github.com>
2023-08-29 13:07:18 +0530

    add api demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+
+const API = "https://jsonplaceholder.typicode.com"
 
 function App() {
+  const [mydata, setMydata] = useState([]);
+  const [iserr, setErr] = useState("");
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts")
+  //     .then((response) => setMydata(response.data))
+  //     .catch((error) => setErr(error.message));
+  // }, []);
+
+  const getApiaData = async (url) => { //url remove from parentis while using .get("/posts") is called
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");  //.get(url) or .get("/posts")
+      setMydata(res.data);
+    } catch (error) {
+      setErr(error.message);
+    }
+  };
+
+  useEffect(() => {
+    // getApiaData(); 
+    getApiaData(`${API}/posts`); // no need write anthing in parentesis
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>data</h1>
+      {iserr !== "" && <h2 style={{ color: "red" }}>{iserr}</h2>}
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>userId</th>
+            <th>title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mydata.slice(0, 12).map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.userId}</td>
+              <td>{item.title}</td>
+              <td>{item.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
