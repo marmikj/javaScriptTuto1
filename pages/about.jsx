055cc359231acda5ae078cadaa27636c7b4977b1
marmikj <140173006+marmikj@users.noreply.github.com>
2023-10-13 14:09:47 +0530

    scss apply in project

---
+++ pages/about.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/about.module.css'
+const about = () => {
+  return (
+    <div className={styles.highlight}>
+      <h2>Hello About Page </h2>
+      {/* <button className="btn btn-success">Primary</button> */}
+
+
+    </div>
+  );
+};
+
+export default about;
